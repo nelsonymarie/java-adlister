2afa2d2dd516bf5c2eca11ac82cbea68e7b45a6f
Misael Cuadrado <104469677+nelsonymarie@users.noreply.github.com>
2023-03-14 17:57:57 -0500

    servlet updates

---
+++ src/main/java/HelloWorld/HelloServlet.java
@@ -0,0 +1,27 @@
+package HelloWorld;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        if (name != null && !name.isEmpty()) {
+            out.println("<h1>Hello, " + name + "!</h1>");
+        } else {
+            out.println("<h1>Hello, World!</h1>");
+        }
+    }
+}
